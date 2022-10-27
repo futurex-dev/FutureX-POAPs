13bb4bfc20837f1b1bc81e50713da879d30ab92f
Jianbai Ye <jianbaiye@outlook.com>
2022-10-27 17:30:06 +0800

    fix: remove goerli temp
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,9 @@ require('@openzeppelin/hardhat-upgrades');
 /** @type import('hardhat/config').HardhatUserConfig */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+// const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.7",
-  // solidity: "0.5.5",
   // gasReporter: {
   //   enabled: (process.env.REPORT_GAS) ? true : false
   // },
@@ -20,9 +19,9 @@ module.exports = {
   allowUnlimitedContractSize: true,
   networks: {
     hardhat: {},
-    goerli: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    }
+    // goerli: {
+    //   url: API_URL,
+    //   accounts: [`0x${PRIVATE_KEY}`]
+    // }
   },
 };
