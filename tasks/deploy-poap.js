69a890c8b82f1152f78691f2d92dc026ed6b924b
Jianbai Ye <jianbaiye@outlook.com>
2022-11-07 21:08:44 +0800

    fix: no baseuri
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- tasks/deploy-poap.js
+++ tasks/deploy-poap.js
@@ -3,14 +3,13 @@ const { task } = require("hardhat/config")
 task("deploy-poap", "upgrade poap contract from the address")
     .setAction(async (taskArgs) => {
         const contract = await ethers.getContractFactory("Poap");
-        const baseURI = "https://futurex.dev/token/";
         const poapName = "FutureXPoap";
         // Start deployment, returning a promise that resolves to a contract object
         const gas_price = await ethers.provider.getGasPrice();
         const estimatedGas = await ethers.provider.estimateGas(contract.getDeployTransaction().data)
         console.log(`Estimate deploy gas fee: ${estimatedGas * gas_price / 1e18} eth with Gas ${estimatedGas} and price ${gas_price / 1e18}`);
 
-        const contracted = await upgrades.deployProxy(contract, [poapName, poapName, baseURI, []], { initializer: '__POAP_init' });
+        const contracted = await upgrades.deployProxy(contract, [poapName, poapName, []], { initializer: '__POAP_init' });
         await contracted.deployed();
         const receipt = await contracted.deployTransaction.wait();
         console.log("Contract deployed to address:", contracted.address);
